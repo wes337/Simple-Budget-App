014f3a99585d54774c20c472f8df8a5f43390cdd
wes337 <wesmoses@gmail.com>
2017-10-22 22:34:13 +0300

    create, update, delete, and load transactions and businesses

--- src/app/transactions/vuex/actions.js
+++ src/app/transactions/vuex/actions.js
@@ -1,3 +1,89 @@
-export const doSomething = ({ commit }, data) => {
-  commit('DO_SOMETHING', { data: data });
+import { guid } from '../../../utils';
+import { deleteTransaction as deleteTransactionFromAPI, deleteBusiness as deleteBusinessFromAPI, fetchBusinesses, fetchTransactions, saveBusiness, saveTransaction } from '../api';
+
+const prepareTransaction = (getters, data) => {
+// code shared by createTransaction and updateTransaction
+// find the budget based on the date
+// we'll go ahead and do this for existing transactions in case the user changes the date
+// in the future we may only want to only do it on a date change
+  let budget = getters.getBudgetByDate(data.date);
+  if (!budget) throw new Error('Could not find a budget for the date ' + data.date);
+  data.budget = budget.id;
+
+// tell the budget category that the transaction is occuring so it can update its amount
+  let budgetCategory = getters.getBudgetCategoryByBudgetAndCategory(budget.id, data.category);
+  if (!budgetCategory) throw new Error('Could not find a budget cateogry for ' + data.category);
+  // don't dispatch yet, we are just preparing data here
+
+  return { preparedData: data, budgetCategory: budgetCategory, budget: budget };
+};
+
+export const createTransaction = ({ commit, dispatch, getters }, data) => {
+  let { preparedData, budgetCategory, budget } = prepareTransaction(getters, data);
+
+  let id = guid();
+  let transaction = Object.assign({ id: id }, preparedData);
+
+  // update the budget category, which updates the budget spend total
+  dispatch('updateBudgetCategorySpent', {
+    budgetCategory: budgetCategory,
+    budget: budget,
+    amount: transaction.amount
+  });
+
+  // update the account balance
+  dispatch('updateAccountBalance', {
+    account: getters.getAccoundById(data.account),
+    amount: transaction.amount
+  });
+
+  commit('CREATE_TRANSACTION', { transaction: transaction });
+  saveTransaction(transaction);
+};
+
+export const updateTransaction = ({ commit, getters }, data) => {
+  // TODO: handle any change the user could make here! including
+  // updating budgets or account balances
+
+  let { preparedData } = prepareTransaction(getters, data);
+
+  commit('UPDATE_TRANSACTION', { transaction: preparedData });
+  saveTransaction(preparedData);
+};
+
+export const deleteTransaction = ({ commit }, data) => {
+  commit('DELETE_TRANSACTION', { transaction: data });
+  deleteTransactionFromAPI(data);
+};
+
+export const loadTransactions = ({ state, commit }) => {
+  // loads transactions if they're not already loaded
+  if (!state.transactions || Object.keys(state.transactions).length === 0) {
+    return fetchTransactions().then((res) => {
+      commit('LOAD_TRANSACTIONS', res);
+    });
+  }
+};
+
+export const createBusiness = ({ commit, state }, data) => {
+  let id = guid();
+  let business = Object.assign({ id: id }, data);
+  commit('CREATE_BUSINESS', { business: business });
+  saveBusiness(business);
+
+  return business;
+};
+
+export const loadBusinsesses = ({ state, commit }) => {
+  // loads businsesses if they're not already loaded
+  if (!state.businesses || Object.keys(state.businesses).length === 0) {
+    return fetchBusinesses().then((res) => {
+      commit('LOAD_BUSINESSES', res);
+    });
+  }
+};
+
+export const deleteBusiness = ({ commit }, data) => {
+  commit('DELETE_BUSINESS', { business: data });
+  deleteBusinessFromAPI(data);
 };
--- src/app/transactions/vuex/mutations.js
+++ src/app/transactions/vuex/mutations.js
@@ -1,5 +1,34 @@
+import Vue from 'Vue';
+
+const forceFloats = (o) => {
+  o.amount = parseFloat(o.amount);
+};
+
 export default {
-  DO_SOMETHING (state, { data }) {
-    state.something = data;
+  CREATE_TRANSACTION (state, payload) {
+    forceFloats(payload.transaction);
+    Vue.set(state.transactions, payload.transaction.id, payload.transaction);
+  },
+
+  UPDATE_TRANSACTION (state, payload) {
+    forceFloats(payload.transaction);
+    state.transactions[payload.transaction.id] = payload.transaction;
+  },
+
+  DELETE_TRANSACTION (state, payload) {
+    Vue.delete(state.transactions, payload.transaction.id);
+  },
+
+  LOAD_TRANSACTIONS (state, payload) {
+    state.transactions = payload;
+    Object.values(state.transactions).forEach((o) => { forceFloats(o); });
+  },
+
+  LOAD_BUSINESSES (state, payload) {
+    state.businesses[payload.business.id] = payload.business;
+  },
+
+  DELETE_BUSINESS (state, payload) {
+    Vue.delete(state.businesses, payload.business.id);
   }
 };
